395501b5029dcbfea02b8bab5bf96383376b6688
alessandropecchini99 <pecchini99@gmail.com>
2023-05-01 20:58:47 +0200

    finish random style

--- script.js
+++ script.js
@@ -1,6 +1,11 @@
 // DESCRIZIONE ESERCIZIO
 // 1. Inserisco in JS un array con i dati del team
-// 2. Mostro a pagina le informazioni che ho inserito nell'array
+// 2 Mostra array in console
+// 3. Stampare le stesse informazioni nel DOM sottoforma di stringhe
+// BONUS
+// 3. Mostro a schermo l'immagine che ho precedentemente dichiarato nell'object
+// 4 Mostro a schermo l'effettiva immagine dichiarata precedentemente come stringa
+// 5. Organizzo le informazioni in card
 
 
 // ------------------------------------------------------ //
@@ -42,9 +47,14 @@ const arrTeam = [
         img: `img/barbara-ramos-graphic-designer.jpg`
     },
 ];
-console.log(arrTeam[1].nome)
 
-// 2. MOSTRO LE INFORMAZIONI A SCHERMO
+// 2. MOSTRO ARRAY IN CONSOLE
+console.log(`Array con i dati degli objects:`)
+console.log(arrTeam)
+
+// 4. TRASFORMO LA IMG STRINGA IN IMMAGINE EFFETTIVA
+// &&
+// 5. INSERISCO LE INFORMAZIONI IN CARD
 for (i = 0; i < arrTeam.length; i++) {
     card_grid.innerHTML += `<div class="single_card">` + `<img src=${arrTeam[i].img}>` + `<div class="info"><h2>${arrTeam[i].nome}</h2>` + `<p>${arrTeam[i].job}</p></div>`
 }
